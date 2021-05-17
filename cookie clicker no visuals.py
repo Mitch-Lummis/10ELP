a260cc199e4306d7a49745fdca047f24b5fa54b6
Mitch-Lummis <79547772+Mitch-Lummis@users.noreply.github.com>
2021-05-17 10:07:40 +1000

    Add files via upload
    This is the one with thread 3

--- cookie clicker no visuals.py
+++ cookie clicker no visuals.py
@@ -1,10 +1,14 @@
-import threading, time
+#import neccessary libraries
+import threading
 from pynput import keyboard
-grandmas, numberCookies, grandmaCookies = 0, 0, 0
+import time
+#set variables
+grandmas = 0
+numberCookies = 0
+grandmaCookies = 0
 clickerLevel = 1
 upgradeCost = 10
 gameContinue = True
-# What are all the int( ) conversions for?
 grandmas = int(grandmas)
 numberCookies = int(numberCookies)
 clickerLevel = int(clickerLevel)
@@ -12,6 +16,7 @@ grandmaCookies = int(grandmaCookies)
 upgradeCost = int(upgradeCost)
 
 def infiniteloop1():
+    #loop for grandmas
     global numberCookies
     global grandmas
     global grandmaCookies
@@ -24,10 +29,19 @@ def infiniteloop1():
         time.sleep(10)
 
 def infiniteloop2():
+    #loop for detecting key press
     with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
             listener.join()
+
+def infiniteloop3():
+    print('thread 3 running')
+    global numberCookies
+    if numberCookies <= -1:
+        thread1.exit()
+        thread2.exit()
+        print('game over!')
 
 def on_press(key):
     try:
@@ -39,17 +53,19 @@ def on_press(key):
         global grandmaCookies
         global time
         global upgradeCost
-        if key.char == 'g':
+        if key.char == 'g': 
+            #gives you another grandma and takes away 10 cookies
             grandmas = grandmas +1
             numberCookies = numberCookies -10
             grandmas = str(grandmas)
             print('''Number of grandma's is: ''' + grandmas)
             grandmas = int(grandmas)
         if key.char == 'u':
+            #upgrades clicker Level and takes away the upgrade cost from your cookies
             clickerLevel = clickerLevel *2         
             numberCookies = numberCookies - upgradeCost
             clickerLevel = int(clickerLevel)
-            upgradeCost = upgradeCost + upgradeCost + clickerLevel
+            upgradeCost = upgradeCost + upgradeCost + clickerLevel *2
             clickerLevel = str(clickerLevel)
             print('Clicker Level is: ' + clickerLevel)
             upgradeCost = str(upgradeCost)
@@ -57,6 +73,7 @@ def on_press(key):
             upgradeCost = int(upgradeCost)
             clickerLevel = int(clickerLevel)
         if key.char == 'm':
+            #claims the cookies grandmas have baked you
             grandmaCookies = str(grandmaCookies)
             print('''Grandma's have currently baked you ''' + grandmaCookies + ' cookies.')
             time.sleep(1)
@@ -67,6 +84,7 @@ def on_press(key):
             print('Number of cookies is: ' + numberCookies)
             numberCookies = int(numberCookies)
         if key.char == 'h':
+            #prints the instructions in case you forget
             print('''Key 'c' to get more cookies ''')
             grandmas = str(grandmas)
             print('''Key 'g' to buy a grandma. You currently have: ''' + grandmas)
@@ -79,6 +97,7 @@ def on_press(key):
             print('''Key 'm' to see how many cookies grandma's have baked and claim them''')
             print('''Key 'h' if you forget controls and need help''')
     except AttributeError:
+        # in case they press the wrong key
         print('special key pressed: {0}'.format(
             key))
 
@@ -89,6 +108,7 @@ def on_release(key):
     '''print('Key released: {0}'.format(
         key))'''
     if key.char == 'c':
+        #adds cookies. It's on release so you can't just hold the key down
         numberCookies = numberCookies + clickerLevel
         numberCookies = str(numberCookies)
         print('Number of cookies is: ' + numberCookies)
@@ -99,10 +119,11 @@ def on_release(key):
 
 print('''press key 'h' for controls''')
 
-# Really cool that you've started looking into threading.
-
 thread1 = threading.Thread(target=infiniteloop1)
 thread1.start()
 
 thread2 = threading.Thread(target=infiniteloop2)
 thread2.start()
+
+thread3 = threading.Thread(target=infiniteloop3)
+thread3.start()
